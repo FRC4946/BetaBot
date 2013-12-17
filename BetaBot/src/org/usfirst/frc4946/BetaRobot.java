ccbf3e927fcdfa3d4bb3a5ef052f3563dacd07e7
Bart Verzijlenberg <bart.verzijlenberg@gmail.com>
2013-12-16 21:08:06 -0500

    Discussion code for group

--- BetaBot/src/org/usfirst/frc4946/BetaRobot.java
+++ BetaBot/src/org/usfirst/frc4946/BetaRobot.java
@@ -8,6 +8,10 @@
 package org.usfirst.frc4946;
 
 
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
 
 /**
@@ -18,17 +22,78 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class BetaRobot extends SimpleRobot {
+	
+	RobotDrive m_robotDrive = new RobotDrive(RobotConstants.PWM_MOTOR_LEFT, RobotConstants.PWM_MOTOR_RIGHT);
+	
+	Joystick m_driveJoystick = new Joystick(RobotConstants.JOYSTICK_LEFT);
+	Joystick m_taskJoystick = new Joystick(RobotConstants.JOYSTICK_RIGHT);
+	
+	PWM m_leftServo = new PWM(RobotConstants.PWM_LEFT_SERVO);
+	PWM m_rightServo = new PWM(RobotConstants.PWM_RIGHT_SERVO);
+	
+	DriverStationLCD m_driverStation = DriverStationLCD.getInstance();
+	
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        int a = 0;
+        
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+    	
+    	m_driverStation.println(DriverStationLCD.Line.kMain6, 1, "Entering operator control");
+    	m_driverStation.updateLCD();
+    	
+    	while (isOperatorControl() && isEnabled()){
+    		//Call the drive oriented code
+    		operatorDriveSystem();
+    		
+    		//Call the task oriented code
+    		operatorTaskSystem();
+    		
+    	}
+    	
+    	m_driverStation.println(DriverStationLCD.Line.kMain6, 1, "Stopping operator control");
+    	m_driverStation.updateLCD();
+    	
     }
+
+	private void operatorTaskSystem() {
+		//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+		//Get seems to return -1 to 1.
+		double x = m_taskJoystick.getX();
+		double y = m_taskJoystick.getY();
+		
+		//Expects a value of 0-1
+		// Shift over by 1 and bring to range
+		m_leftServo.setPosition((x+1.0)/2.0);
+		m_rightServo.setPosition((y+1.0)/2.0);
+	}
+
+	private void operatorDriveSystem() {
+		//m_robotDrive.arcadeDrive(m_DriveJoystick); 
+		
+		//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+		//Same as above, but reduce either turning, or maximum speed depending on the trigger.
+		double outputMagnitude = m_driveJoystick.getY();
+		double curve = m_driveJoystick.getX();
+		
+		//Check the trigger
+		if( m_driveJoystick.getTrigger() ){
+			//allow fast speed, but reduce turning
+			curve *= 0.25;
+			
+		}else{
+			//Drive slow if the trigger is not down
+			outputMagnitude *= 0.25;
+			
+		}
+		
+		m_robotDrive.arcadeDrive(outputMagnitude, curve, true);
+		
+	}
 }
---
+++ BetaBot/src/org/usfirst/frc4946/RobotConstants.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc4946;
+
+public class RobotConstants {
+	final static int PWM_MOTOR_LEFT = 1;
+	final static int PWM_MOTOR_RIGHT = 2;
+	
+	public static final int JOYSTICK_LEFT = 1;
+	public static final int JOYSTICK_RIGHT = 2;
+	
+	public static final int PWM_LEFT_SERVO = 9;
+	public static final int PWM_RIGHT_SERVO = 10;
+	
+}
