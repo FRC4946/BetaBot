e5aa6d4e4adc15da62e98ada578449f5b5279d16
bverzijl <bverzijl@CA02LA0004.ds.mda.ca>
2014-02-13 16:56:57 -0500

    Initial commit

---
+++ src/org/usfirst/frc4946/IntakeArm.java
@@ -0,0 +1,113 @@
+package org.usfirst.frc4946;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class IntakeArm {
+
+	private SpeedController m_intakeController = new Jaguar(RobotConstants.PWM_MOTOR_INTAKE);
+
+	private Solenoid m_extendGrabberSolenoid = new Solenoid(RobotConstants.EXTEND_ARM_VALVE_RELAY);
+	private Solenoid m_retractGrabberSolenoid = new Solenoid(RobotConstants.RETRACT_ARM_VALVE_RELAY);
+
+	private double speed = 0.0;
+	private boolean motorsAreEnabled = false;
+	private boolean armIsExtended = false;
+
+	private int m_solenoidCounter = 0;
+
+	
+	
+	
+	public void toggleExtended() {
+		armIsExtended = !armIsExtended;
+
+		setExtended(armIsExtended);
+	}
+
+	/**
+	 * Extend or retract the arm.
+	 * 
+	 * @param isExtended
+	 *            Whether to extend or retract the arm.
+	 */
+	public void setExtended(boolean isExtended) {
+
+		armIsExtended = isExtended;
+
+		if (isExtended) {
+			m_retractGrabberSolenoid.set(false);
+			m_extendGrabberSolenoid.set(true);
+			m_solenoidCounter = RobotConstants.SOLENOID_COOLDOWN_TIME;
+
+		} else {
+			m_extendGrabberSolenoid.set(false);
+			m_retractGrabberSolenoid.set(true);
+			m_solenoidCounter = RobotConstants.SOLENOID_COOLDOWN_TIME;
+		}
+	}
+	
+	public void updateSolenoids(){
+		// Turn off the solenoid after 100 cycles.
+		if (m_solenoidCounter == 0) {
+			m_extendGrabberSolenoid.set(false);
+			m_retractGrabberSolenoid.set(false);
+
+		} else {
+			m_solenoidCounter--;
+
+		}
+	}
+	
+	public boolean getExtendedState(){
+		return armIsExtended;
+	}
+	
+	
+	
+	
+	
+
+	public void toggleEnabled() {
+		motorsAreEnabled = !motorsAreEnabled;
+
+		setEnabled(motorsAreEnabled);
+	}
+
+	/**
+	 * Start or stop the motors. If the speed has been changed, call
+	 * setEnabled(true) to update the motors.
+	 * 
+	 * @param isEnabled
+	 *            Whether to enable the motors or not.
+	 */
+	public void setEnabled(boolean isEnabled) {
+
+		motorsAreEnabled = isEnabled;
+
+		if (isEnabled) {
+			m_intakeController.set(1.0);
+		} else {
+			m_intakeController.set(0.0);
+		}
+
+	}
+
+	/**
+	 * Set the speed of the motors without checking the result. After calling
+	 * this, call setEnabled(true) to update the motors.
+	 * 
+	 * @param power
+	 *            The speed to set. Should be between -1.0 and 1.0
+	 */
+	public void setSpeedOpenLoop(double power) {
+
+		// Make sure that the value is within the valid range of -1 to 1
+		if (power >= -1.0 && power <= 1.0) {
+			speed = power;
+		}
+	}
+
+}
---
+++ src/org/usfirst/frc4946/RobotConstants.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc4946;
+
+public class RobotConstants {
+	
+		//*** Motors ***\\
+	
+        final static int PWM_MOTOR_LEFT_FRONT = 1;
+        final static int PWM_MOTOR_LEFT_REAR = 2;
+        final static int PWM_MOTOR_RIGHT_FRONT = 3;
+        final static int PWM_MOTOR_RIGHT_REAR = 4;
+        
+        final static int PWM_MOTOR_LAUNCHER_TOP = 7;
+        final static int PWM_MOTOR_LAUNCHER_BOTTOM = 6;
+        
+        final static int PWM_MOTOR_INTAKE = 5;
+
+        
+        
+        //*** Joysticks ***\\
+        
+        public static final int JOYSTICK_LEFT = 1;
+        public static final int JOYSTICK_RIGHT = 2;
+        
+        public static final double DRIVE_JOYSTICK_DEADZONE = 0.1;
+        public static final double TASK_JOYSTICK_DEADZONE = 0.1;
+        
+        
+        //*** Pneumatics ***\\
+        
+        public static final int COMPRESSOR_PRESSURE_SWITCH = 1; // On the GPIO header of the digital sidecar
+        public static final int COMPRESSOR_RELAY = 1;			// On the relay header of the digital sidecar
+        
+        public static final int EXTEND_ARM_VALVE_RELAY = 1; //The valve that extends the pistons on the intake arm
+        public static final int RETRACT_ARM_VALVE_RELAY = 2; //The valve that retracts the pistons on the intake arm
+        
+        public static final int BALL_LIFT_VALVE_RELAY = 3; // The valve to raise the ball lifter. (Purple in the diagram)
+        public static final int BALL_LOWER_VALVE_RELAY = 4; // The valve to lower the ball lifter
+        
+        
+        
+        
+        //*** Buttons ***\\
+        
+        public static int JOYSTICK_BUTTON_INTAKE = 2;
+        public static int JOYSTICK_BUTTON_LAUNCH = 3;
+        public static int JOYSTICK_BUTTON_INTAKE_ROLLER = 4;
+        public static int JOYSTICK_BUTTON_SHOOT_ORIENTATION = 5;
+        public static int JOYSTICK_BUTTON_INTAKE_ORIENTATION = 6;        
+        
+        
+        
+        //*** Misc ***\\
+        
+        public static int SOLENOID_COOLDOWN_TIME = 1000;
+        public static int CONSOLE_UPDATE_TIME = 500;
+}
