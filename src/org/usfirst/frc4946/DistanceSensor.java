ebf38dbc3b8bbd88911d8542f50bb8db0856afdb
Stefan Pleava <stefan_pleava1@hotmail.com>
2014-02-14 22:53:30 -0500

    Commented code

--- src/org/usfirst/frc4946/DistanceSensor.java
+++ src/org/usfirst/frc4946/DistanceSensor.java
@@ -14,9 +14,9 @@ import edu.wpi.first.wpilibj.AnalogChannel;
  * @author Stefan
  */
 public class DistanceSensor {
-    AnalogChannel  m_distanceSensor =  new AnalogChannel (1);
+    AnalogChannel  m_distanceSensor =  new AnalogChannel (RobotConstants.DISTANCE_SENSOR);
     double range=0;
-    double volt=0;
+    double voltage=0;
     public DistanceSensor(){
         
     }
@@ -25,8 +25,14 @@ public class DistanceSensor {
     }
     public double getRangeInchs(){
         
-    volt = m_distanceSensor.getVoltage();
-    range = volt/0.009766;
+    voltage = m_distanceSensor.getVoltage();
+    //at 5v it's 4.9 millivolts per 2cm
+    //so 4.9/2 = 2.45 mv per 1 cm
+    // 2.45mv = 0.00245v per cm
+    // we want inches cause we know field size in inches (1 inch = 2.54 cm
+    // 2.54*0.00245=0.006223
+    //value I found on chief delphi, sensor might not return 0 volt so we might need to add a value to the calculated one above
+    range = voltage/0.006223;
     return range;
     }
 }
