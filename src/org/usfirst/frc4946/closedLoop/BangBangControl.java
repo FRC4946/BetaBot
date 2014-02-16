f740b4e36cc72a6b8145f83da8fb71aa318d9253
Stefan <Stefan@Stefan-Laptop>
2014-02-16 14:22:17 -0500

    bang bang tweak

--- src/org/usfirst/frc4946/closedLoop/BangBangControl.java
+++ src/org/usfirst/frc4946/closedLoop/BangBangControl.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.usfirst.frc4946.closedLoop;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -14,41 +13,46 @@ import org.usfirst.frc4946.RateCounter;
  *
  * @author bart
  */
-public class BangBangControl extends VelocityControl{
-    
+public class BangBangControl extends VelocityControl {
+
     private final DriverStation m_driverStation = DriverStation.getInstance();
     private boolean m_enabled = false;
-    
+
     public BangBangControl(RateCounter counter, SpeedController ctrl) {
         super(counter, ctrl);
-        
+
     }
 
     public void enable(boolean enable) {
         m_enabled = enable;
-        
+
     }
 
     public void update() {
         double curSpeed = m_counter.getRPM();
-        
+
         double fullPower = 11.0 / m_driverStation.getBatteryVoltage();
-       if (m_isReversed){
-           fullPower*=-1;
-       }
-        if( m_enabled){
-            if (curSpeed < m_targetRPM ) {
+        double lowPower = 1;
+
+        if (m_isReversed) {
+            fullPower *= -1;
+            lowPower *= 1;
+
+        }
+
+        if (m_enabled) {
+            if (curSpeed < m_targetRPM) {
                 m_speedControl.set(fullPower);
 
             } else {
                 //Consider changing to a partial command, to prevent brakes coming on...
-                m_speedControl.set(0);
+                m_speedControl.set(lowPower);
 
             }
-        }else{
+        } else {
             //If not enabled, stop the motor.
             m_speedControl.set(0);
-        
+
         }
     }
 }
